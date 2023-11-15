5c44985bf21079338905615f78f855e7de794cc2
GreiceKCGM <greicekellybc@gmail.com>
2023-11-15 18:14:44 -0300

    Criando uma função para a tabela de vorias

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const tabelaVitorias = [
-    { minVitorias: 0, maxVitorias: 10, descricao: "Ferro" },
-    { minVitorias: 11, maxVitorias: 20, descricao: "Bronze" },
-    { minVitorias: 21, maxVitorias: 50, descricao: "Prata" },
-    { minVitorias: 51, maxVitorias: 80, descricao: "Ouro" },
-    { minVitorias: 81, maxVitorias: 90, descricao: "Diamante" },
-    { minVitorias: 91, maxVitorias: 100, descricao: "Lendário" },
-    { minVitorias: 101, maxVitorias: Infinity, descricao: "Imortal" }
-];
+
+let vitorias = 5000;
+
+let derrotas = 3000;
 
 
-let vitorias = 50 ;
-let derrotas = 15;
+function tabelaVitorias( vitorias) {
+    if (vitorias < 10 ) return ('Ferro')
+    if (vitorias >= 11 && vitorias <= 20) return ('Bronze')
+    if (vitorias >= 21 && vitorias <= 50) return ('Prata')
+    if (vitorias >= 51 && vitorias <= 80) return ('ouro')
+    if (vitorias >= 81 && vitorias <= 90) return ('Diamante')
+    if (vitorias >= 91 && vitorias <= 100) return ('Lendario')
+    if (vitorias >= 101) return (' Imortal ')
+  }
 
 
 function calculadoraVitorias(vitorias, derrotas){
@@ -23,14 +25,27 @@ let resultadoVitorias = calculadoraVitorias(vitorias, derrotas)
 resultadoVitorias = parseInt(resultadoVitorias);
 
 
-let nivel;
+console.log(`O Herói tem saldo de ${resultadoVitorias} e está no nível de ${tabelaVitorias(resultadoVitorias)}`)
 
-for (let i = 0; i < tabelaVitorias.length; i++) {
+
+/*const tabelaVitorias = [
+    { minVitorias: 0, maxVitorias: 10, descricao: "Ferro" },
+    { minVitorias: 11, maxVitorias: 20, descricao: "Bronze" },
+    { minVitorias: 21, maxVitorias: 50, descricao: "Prata" },
+    { minVitorias: 51, maxVitorias: 80, descricao: "Ouro" },
+    { minVitorias: 81, maxVitorias: 90, descricao: "Diamante" },
+    { minVitorias: 91, maxVitorias: 100, descricao: "Lendário" },
+    { minVitorias: 101, maxVitorias: Infinity, descricao: "Imortal" }
+];*/
+
+//let nivel;
+
+/*for (let i = 0; i < tabelaVitorias.length; i++) {
     if (resultadoVitorias >= tabelaVitorias[i].minVitorias && resultadoVitorias <= tabelaVitorias[i].maxVitorias) {
         nivel = tabelaVitorias[i].descricao;
         break; 
     }
-}
+}*/
 
 
-console.log("O Herói tem saldo de  " + resultadoVitorias + " está no nível de " + nivel);
+//console.log("O Herói tem saldo de  " + resultadoVitorias + " está no nível de " + nivel);
